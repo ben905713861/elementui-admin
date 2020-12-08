ae1cb2ebbdcd13635b10c3b4118c99190348354f
ben <905713861@qq.com>
2020-12-08 17:53:41 +0800

    -

---
+++ src/views/Activity.vue
@@ -0,0 +1,292 @@
+<style>
+
+</style>
+
+
+<template>
+	<div>
+		<el-form :model="queryParams" ref="searchForm" label-width="auto" :inline="true" @submit.native.prevent="">
+			<el-form-item>
+				<el-button type="success" @click="openDialog()" icon="el-icon-plus">新增</el-button>
+			</el-form-item>
+			<el-form-item prop="name">
+				<el-input type="text" v-model="queryParams.name" placeholder="活动名"></el-input>
+			</el-form-item>
+			<el-form-item prop="wxId">
+				<el-select v-model="queryParams.wxId" clearable="" placeholder="微信公众号">
+					<el-option v-for="wx in wxMap"
+						:label="wx.name"
+						:key="wx.wxId"
+						:value="wx.wxId">
+					</el-option>
+				</el-select>
+			</el-form-item>
+
+			<el-form-item>
+				<el-button type="primary" native-type="submit" @click="search(true)" icon="el-icon-search">搜索</el-button>
+				<el-button @click="resetForm('searchForm')" icon="el-icon-circle-close">重置</el-button>
+			</el-form-item>
+		</el-form>
+
+		<el-table :data="queryResult.rows" stripe="" border="" header-cell-class-name="bg-gray">
+			<el-table-column prop="activityId" label="activityId" align="center" width="80"></el-table-column>
+			<el-table-column prop="name" label="活动名" align="center"></el-table-column>
+			<el-table-column prop="type" label="活动类型" align="center"></el-table-column>
+			<el-table-column prop="allowType" label="限定参与方式" align="center"></el-table-column>
+			<el-table-column label="绑定公众号" align="center">
+				<template slot-scope="scope">
+					<template v-if="scope.row.wxDTO">{{ scope.row.wxDTO.name }}</template>
+				</template>
+			</el-table-column>
+			<el-table-column prop="isTest" label="是否为测试模式" :formatter="formatBoolean" align="center"></el-table-column>
+			<el-table-column prop="startTime" label="开始时间" align="center"></el-table-column>
+			<el-table-column prop="endTime" label="结束时间" align="center"></el-table-column>
+			<el-table-column label="开展状态" :formatter="formatStatus" align="center"></el-table-column>
+			<el-table-column prop="addTime" label="创建时间" align="center"></el-table-column>
+			<el-table-column align="center" label="操作" width="200">
+				<template slot-scope="scope">
+					<el-button size="mini" type="warning" @click="openDialog(scope.row.activityId)" icon="el-icon-edit-outline">修改</el-button>
+					<el-button size="mini" type="danger" @click="deleteData(scope.row.activityId)" icon="el-icon-delete">删除</el-button>
+				</template>
+			</el-table-column>
+		</el-table>
+
+		<el-pagination class="pagination"
+			background=""
+			layout="total,prev,pager,next,sizes"
+			:total="queryResult.total"
+			:current-page="queryParams.page"
+			:page-size="queryParams.limit"
+			@size-change="sizeChange"
+			@current-change="currentChange">
+		</el-pagination>
+
+		<el-dialog title="编辑活动信息"
+			:visible.sync="updateDialog"
+			:close-on-click-modal="false"
+			@close="resetForm('updateForm')"
+			width="30%">
+			<el-form :model="activityDTO" :rules="updateRules" ref="updateForm" label-width="auto">
+				<el-form-item prop="activityId" v-show="false">
+					<el-input v-model="activityDTO.activityId"></el-input>
+				</el-form-item>
+				<el-form-item label="活动名" prop="name">
+					<el-input v-model="activityDTO.name"></el-input>
+				</el-form-item>
+				<el-form-item label="类型" prop="type" placeholder="活动类型">
+					<el-select v-model="activityDTO.type" multiple="">
+						<el-option label="问卷" key="问卷" value="问卷"></el-option>
+						<el-option label="抽奖" key="抽奖" value="抽奖"></el-option>
+						<el-option label="登记" key="登记" value="登记"></el-option>
+						<el-option label="投票" key="投票" value="投票"></el-option>
+					</el-select>
+				</el-form-item>
+				<el-form-item label="是否为测试模式" prop="isTest">
+					<el-switch v-model="activityDTO.isTest"></el-switch>
+				</el-form-item>
+				<el-form-item label="开始时间" prop="startTime">
+					<el-date-picker v-model="activityDTO.startTime"
+						type="datetime"
+						placeholder="选择开始时间"
+						value-format="yyyy-MM-dd HH:mm:ss">
+					</el-date-picker>
+				</el-form-item>
+				<el-form-item label="结束时间" prop="endTime">
+					<el-date-picker v-model="activityDTO.endTime"
+						type="datetime"
+						placeholder="选择结束时间"
+						value-format="yyyy-MM-dd HH:mm:ss">
+					</el-date-picker>
+				</el-form-item>
+				<el-form-item label="限定参与方式" prop="allowType">
+					<el-select v-model="activityDTO.allowType" placeholder="限定参与方式">
+						<el-option label="无" key="无" value="无"></el-option>
+						<el-option label="微信" key="微信" value="微信"></el-option>
+						<el-option label="手机" key="手机" value="手机"></el-option>
+					</el-select>
+				</el-form-item>
+				<el-form-item label="绑定公众号" prop="wxId">
+					<el-select v-model="activityDTO.wxId" placeholder="微信公众号">
+						<el-option label="不绑定" :key="0" :value="0"></el-option>
+						<el-option v-for="wx in wxMap"
+							:label="wx.name"
+							:key="wx.wxId"
+							:value="wx.wxId">
+						</el-option>
+					</el-select>
+				</el-form-item>
+				
+				
+			</el-form>
+			<span slot="footer">
+				<el-button type="success" @click="saveData()">保存</el-button>
+			</span>
+		</el-dialog>
+
+	</div>
+</template>
+
+
+<script>
+import { Message } from "element-ui";
+import http from '@/components/Http';
+
+export default {
+	data() {
+	  return {
+	  	wxMap: {},
+		queryParams: {
+			name: '',
+			appId: '',
+			page: 1,
+			limit: 10,
+		},
+		queryResult: {
+			rows: [],
+			total: 0,
+		},
+	  	activityDTO: {
+	  		activityId: null,
+	  		name: '',
+	  		type: [],
+	  		isTest: true,
+	  		startTime: '',
+	  		endTIme: '',
+	  		allowType: '无',
+	  		wxId:0,
+	  	},
+	  	updateRules: {
+	  		name: [
+				{required: true, message: "请输入活动名"},
+				{min: 2, max: 50, message: "长度在2-50之间"},
+			],
+			type: [
+				{required: true, message: "请选择类型"},
+			],
+			startTime: [
+				{required: true, message: "请选择开始时间"},
+			],
+			endTime: [
+				{required: true, message: "请选择结束时间"},
+			],
+			allowType: [
+				{required: true, message: "请选择限定参与方式"},
+			],
+	  	},
+	  	updateDialog: false,
+	  }
+	},
+	mounted() {
+		this.search(true);
+		http.ajax('/service-wx/wx/all', {
+			data: this.queryParams,
+			truefun: res => {
+				res.forEach(wx => {
+					this.wxMap[wx.wxId] = wx;
+				});
+			},
+		});
+	},
+	methods: {
+		search(isPage1 = false) {
+			if(isPage1) {
+				this.queryParams.page = 1;
+			}
+			http.ajax('/service-activity/activity', {
+				data: this.queryParams,
+				truefun: res => {
+					this.queryResult = res;
+				},
+			});
+		},
+		resetForm(formName) {
+			this.$refs[formName].resetFields();
+		},
+		sizeChange(limit) {
+			this.queryParams.limit = limit;
+			this.search();
+		},
+		currentChange(page) {
+			this.queryParams.page = page;
+			this.search();
+		},
+		openDialog(activityId) {
+			if(activityId != undefined) {
+				http.ajax('/service-activity/activity/' + activityId, {
+					truefun: resData => {
+						resData.type = resData.type.split(',');
+						this.activityDTO = resData;
+					},
+				});
+			}
+			this.updateDialog = true;
+		},
+		saveData() {
+			this.$refs['updateForm'].validate(valid => {
+				if(!valid) {
+					Message.error('表单信息错误');
+					return;
+				}
+				http.ajax('/service-activity/activity', {
+					method: this.activityDTO.activityId ? 'put' : 'post',
+					data: this.activityDTO,
+					truefun: resData => {
+						this.updateDialog = false;
+						this.search(true);
+					},
+				});
+			});
+		},
+		deleteData(activityId) {
+			this.$confirm('确定删除？', '操作警告')
+			.then(() => {
+				http.ajax('/service-activity/activity/' + activityId, {
+					method: 'delete',
+					truefun: resData => {
+						this.search();
+					},
+				});
+			})
+			.catch(() => {});
+		},
+		formatBoolean(row, column, value) {
+			return value ? '是' : '否';
+		},
+		formatStatus(row, column, value) {
+			let status;
+			let now;
+			{
+				var date = new Date();
+				var Y = date.getFullYear();
+				var M = date.getMonth() + 1;
+				var D = date.getDate();
+				if(D < 10) {
+					D = '0' + D;
+				}
+				var h = date.getHours();
+				if(h < 10) {
+					h = '0' + h;
+				}
+				var m = date.getMinutes();
+				if(m < 10) {
+					m = '0' + m;
+				}
+				var s = date.getSeconds();
+				if(s < 10) {
+					s = '0' + s;
+				}
+			}
+			now = Y+'-'+M+'-'+D+' '+h+':'+m+':'+s;
+			if(now < row.startTime) {
+				status = '未开始';
+			} else if(now > row.endTime) {
+				status = '已结束';
+			} else {
+				status = '进行中';
+			}
+			row.status = status;
+			return status;
+		},
+	},
+}
+</script>
---
+++ src/views/Weixin.vue
@@ -0,0 +1,207 @@
+<style>
+
+</style>
+
+
+<template>
+	<div>
+		<el-form :model="queryParams" ref="searchForm" label-width="auto" :inline="true" @submit.native.prevent="">
+			<el-form-item>
+				<el-button type="success" @click="openDialog()" icon="el-icon-plus">新增</el-button>
+			</el-form-item>
+			<el-form-item prop="name">
+				<el-input type="text" v-model="queryParams.name" placeholder="公众号名"></el-input>
+			</el-form-item>
+			<el-form-item prop="appId">
+				<el-input type="text" v-model="queryParams.appId" placeholder="appId"></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" native-type="submit" @click="search(true)" icon="el-icon-search">搜索</el-button>
+				<el-button @click="resetForm('searchForm')" icon="el-icon-circle-close">重置</el-button>
+			</el-form-item>
+		</el-form>
+
+		<el-table :data="queryResult.rows" stripe="" border="" header-cell-class-name="bg-gray">
+			<el-table-column prop="wxId" label="wxId" align="center" width="80"></el-table-column>
+			<el-table-column prop="name" label="公众号名" align="center"></el-table-column>
+			<el-table-column prop="appId" label="appId" align="center"></el-table-column>
+			<el-table-column prop="appSecret" label="appSecret" align="center"></el-table-column>
+			<el-table-column prop="getTokenUrl" label="第三方获取accessToken接口" align="center"></el-table-column>
+			<el-table-column prop="getTokenParam" label="第三方获取accessToken字段名" align="center"></el-table-column>
+			<el-table-column align="center" label="操作" width="200">
+				<template slot-scope="scope">
+					<el-button size="mini" type="warning" @click="openDialog(scope.row.wxId)" icon="el-icon-edit-outline">修改</el-button>
+					<el-button size="mini" type="danger" @click="deleteData(scope.row.wxId)" icon="el-icon-delete">删除</el-button>
+				</template>
+			</el-table-column>
+		</el-table>
+
+		<el-pagination class="pagination"
+			background=""
+			layout="total,prev,pager,next,sizes"
+			:total="queryResult.total"
+			:current-page="queryParams.page"
+			:page-size="queryParams.limit"
+			@size-change="sizeChange"
+			@current-change="currentChange">
+		</el-pagination>
+
+		<el-dialog title="编辑管理员信息"
+			:visible.sync="updateDialog"
+			:close-on-click-modal="false"
+			@close="resetForm('updateForm')"
+			width="30%">
+			<el-form :model="wxDTO" :rules="updateRules" ref="updateForm" label-width="auto">
+				<el-form-item prop="wxId" v-show="false">
+					<el-input v-model="wxDTO.wxId"></el-input>
+				</el-form-item>
+				<el-form-item label="公众号名" prop="name">
+					<el-input v-model="wxDTO.name"></el-input>
+				</el-form-item>
+				<el-form-item label="appId" prop="appId">
+					<el-input v-model="wxDTO.appId"></el-input>
+				</el-form-item>
+				<el-form-item label="appSecret" prop="appSecret">
+					<el-input v-model="wxDTO.appSecret"></el-input>
+				</el-form-item>
+				<el-form-item label="第三方获取accessToken接口" prop="getTokenUrl">
+					<el-input v-model="wxDTO.getTokenUrl"></el-input>
+				</el-form-item>
+				<el-form-item label="第三方获取accessToken字段名" prop="getTokenParam">
+					<el-input v-model="wxDTO.getTokenParam"></el-input>
+				</el-form-item>
+			</el-form>
+			<span slot="footer">
+				<el-button type="success" @click="saveData()">保存</el-button>
+			</span>
+		</el-dialog>
+
+	</div>
+</template>
+
+
+<script>
+import { Message } from "element-ui";
+import http from '@/components/Http';
+
+export default {
+	data() {
+	  return {
+		queryParams: {
+			name: '',
+			appId: '',
+			page: 1,
+			limit: 10,
+		},
+		queryResult: {
+			rows: [],
+			total: 0,
+		},
+	  	wxDTO: {
+	  		wxId: null,
+	  		name: '',
+	  		appId: '',
+	  		appSecret: '',
+	  		getTokenUrl: '',
+	  		getTokenParam: '',
+	  	},
+	  	updateRules: {
+	  		name: [
+				{required: true, message: "请输入公众号名"},
+				{min: 2, max: 20, message: "长度在2-20之间"},
+			],
+			appId: [
+				{required: true, message: "请输入appId"},
+				{min: 18, max: 18, message: "长度必须是18位"},
+			],
+			appSecret: [
+				{required: true, message: "请输入appSecret"},
+				{min: 32, max: 32, message: "长度必须是32位"},
+			],
+			getTokenUrl: [
+				{min: 0, max: 200, message: "第三方获取accessToken接口必须是200位以内"},
+			],
+			getTokenParam: [
+				{min: 0, max: 20, message: "第三方获取accessToken字段名必须是20位以内"},
+			],
+	  	},
+	  	updateDialog: false,
+	  }
+	},
+	mounted() {
+		this.search(true);
+	},
+	methods: {
+		search(isPage1 = false) {
+			if(isPage1) {
+				this.queryParams.page = 1;
+			}
+			http.ajax('/service-activity/wx', {
+				data: this.queryParams,
+				truefun: res => {
+					this.queryResult = res;
+				},
+			});
+		},
+		resetForm(formName) {
+			this.$refs[formName].resetFields();
+		},
+		sizeChange(limit) {
+			this.queryParams.limit = limit;
+			this.search();
+		},
+		currentChange(page) {
+			this.queryParams.page = page;
+			this.search();
+		},
+		openDialog(wxId) {
+			if(wxId != undefined) {
+				http.ajax('/service-activity/wx/' + wxId, {
+					truefun: resData => {
+						this.wxDTO = resData;
+					},
+				});
+			}
+			this.updateDialog = true;
+		},
+		saveData() {
+			this.$refs['updateForm'].validate(valid => {
+				if(!valid) {
+					Message.error('表单信息错误');
+					return;
+				}
+				http.ajax('/service-activity/wx', {
+					method: this.wxDTO.wxId ? 'put' : 'post',
+					data: this.wxDTO,
+					truefun: resData => {
+						this.updateDialog = false;
+						this.search(true);
+					},
+				});
+			});
+		},
+		deleteData(wxId) {
+			this.$confirm('确定删除？', '操作警告')
+			.then(() => {
+				http.ajax('/service-activity/wx/' + wxId, {
+					method: 'delete',
+					truefun: resData => {
+						this.search();
+					},
+				});
+			})
+			.catch(() => {});
+		},
+		formatBoolean(row, column, value) {
+			return value ? '是' : '否';
+		},
+		formatRole(row, column, value) {
+			var roleNames = [];
+			value.forEach(roleId => {
+				roleNames.push(this.roleMap[roleId].name);
+			});
+			return roleNames.join(',');
+		},
+	},
+}
+</script>
